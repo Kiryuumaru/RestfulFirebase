8b04614378f3b0e4adf11883839bf2015b4a2138
Kiryuumaru <kiryuumaru@outlook.com>
2021-04-12 16:57:02 +0800

    .

--- RestfulFirebase/Common/Models/PropertyHolder.cs
+++ RestfulFirebase/Common/Models/PropertyHolder.cs
@@ -6,9 +6,23 @@ namespace RestfulFirebase.Common.Models
 {
     public class PropertyHolder
     {
-        public DistinctProperty Property { get; set; }
-        public string Group { get; set; }
-        public string PropertyName { get; set; }
+        public class PropertyHolderFactory
+        {
+            public PropertyHolder this[string key] { get => Get(key); }
+            public Func<(PropertyHolder PropertyHolder, string Tag), bool> Set { get; private set; }
+            public Func<string, PropertyHolder> Get { get; private set; }
+            public PropertyHolderFactory(
+                Func<(PropertyHolder PropertyHolder, string Tag), bool> set,
+                Func<string, PropertyHolder> get)
+            {
+                Set = set;
+                Get = get;
+            }
+        }
+
+        public DistinctProperty Property { get; private set; }
+        public string Group { get; private set; }
+        public string PropertyName { get; private set; }
         public PropertyHolder(DistinctProperty property, string group, string propertyName)
         {
             Property = property;
