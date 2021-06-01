04ef17554bffc6befd8adb5012bac2accf2bf178
Kiryuumaru <kiryuumaru@outlook.com>
2021-06-01 22:41:08 +0800

    Bug fixes

--- RestfulFirebase/Database/Models/FirebasePropertyDictionary.cs
+++ RestfulFirebase/Database/Models/FirebasePropertyDictionary.cs
@@ -244,6 +244,7 @@ namespace RestfulFirebase.Database.Models
                             }
                         };
                         ModelWire.RealtimeInstance.Child(key).SubModel(item);
+                        Add(key, item);
                     }
                 }
             });
