a4b34578709bfe73f01485e5843045c8bcf552e7
Kiryuumaru <kiryuumaru@outlook.com>
2022-09-10 04:19:54 +0800

    Re arranged file structure

--- Examples/ConsoleAppExample/Program.cs
+++ Examples/ConsoleAppExample/Program.cs
@@ -5,22 +5,30 @@ using RestfulFirebase.Authentication.Requests;
 
 FirebaseConfig config = Credentials.Config(); // Your config
 
-FirebaseUser newUser = await FirebaseAuthentication.CreateUserWithEmailAndPassword(new CreateUserWithEmailAndPasswordRequest()
+//FirebaseUser newUser = await FirebaseAuthentication.CreateUserWithEmailAndPassword(new CreateUserWithEmailAndPasswordRequest()
+//{
+//    Config = config,
+//    Email = "t@st.com",
+//    Password = "123123",
+//});
+
+//Console.WriteLine(newUser.Email);
+//Console.WriteLine(newUser.LocalId);
+
+FirebaseUser user1 = await FirebaseAuthentication.SignInWithEmailAndPassword(new SignInWithEmailAndPasswordRequest()
 {
     Config = config,
     Email = "t@st.com",
     Password = "123123",
 });
 
-Console.WriteLine(newUser.Email);
-Console.WriteLine(newUser.LocalId);
+Console.WriteLine(user1.Email);
+Console.WriteLine(user1.LocalId);
 
-FirebaseUser user = await FirebaseAuthentication.SignInWithEmailAndPassword(new SignInWithEmailAndPasswordRequest()
+FirebaseUser user2 = await FirebaseAuthentication.SignInAnonymously(new AuthenticationRequest()
 {
     Config = config,
-    Email = "t@st.com",
-    Password = "123123",
 });
 
-Console.WriteLine(user.Email);
-Console.WriteLine(user.LocalId);
+Console.WriteLine(user2.Email);
+Console.WriteLine(user2.LocalId);
--- full_clean.bat
+++ full_clean.bat
@@ -1,10 +1,6 @@
 
-rmdir /s /q "%cd%\Src\RestfulFirebase\bin"
-rmdir /s /q "%cd%\Src\RestfulFirebase\obj"
-rmdir /s /q "%cd%\Tests\RestfulFirebase.Playground\bin"
-rmdir /s /q "%cd%\Tests\RestfulFirebase.Playground\obj"
-rmdir /s /q "%cd%\Tests\RestfulFirebase.TestCore\bin"
-rmdir /s /q "%cd%\Tests\RestfulFirebase.TestCore\obj"
+rmdir /s /q "%cd%\RestfulFirebase\bin"
+rmdir /s /q "%cd%\RestfulFirebase\obj"
 rmdir /s /q "%cd%\Tests\RestfulFirebase.UnitTest\bin"
 rmdir /s /q "%cd%\Tests\RestfulFirebase.UnitTest\obj"
 rmdir /s /q "%cd%\.vs"
