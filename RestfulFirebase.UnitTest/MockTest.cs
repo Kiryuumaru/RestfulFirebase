9b093184d290d4debe79449fcc78a9767d4e20eb
Kiryuumaru <kiryuumaru@outlook.com>
2022-09-11 02:17:05 +0800

    CI CD Trigger

--- RestfulFirebase.UnitTest/MockTest.cs
+++ RestfulFirebase.UnitTest/MockTest.cs
@@ -5,10 +5,22 @@ using RestfulFirebase.Authentication.Exceptions;
 using RestfulFirebase.Authentication.Requests;
 using RestfulFirebase.CloudFirestore.Requests;
 using RestfulFirebase.Common.Requests;
+using RestfulFirebase.FirestoreDatabase;
+using System.Text.Json.Serialization;
 using Xunit;
 
 namespace RestfulFirebase.UnitTest
 {
+    public class Person
+    {
+        public string? FirstName { get; set; }
+
+        public string? LastName { get; set; }
+
+        [JsonPropertyName("value1")]
+        public string? Others { get; set; }
+    }
+
     public class MockTest
     {
         [Fact]
@@ -18,6 +30,22 @@ namespace RestfulFirebase.UnitTest
 
             FirebaseUser user;
 
+            Document<Person>? person = await Api.FirestoreDatabase.GetDocument(new GetDocumentRequest<Person>()
+            {
+                Config = config,
+                Reference = Api.FirestoreDatabase.Database()
+                    .Collection("public")
+                    .Document("sample")
+            });
+
+            await Api.FirestoreDatabase.PatchDocument(new PatchDocumentRequest<Person>()
+            {
+                Config = config,
+                Model = person.Model,
+                Reference = Api.FirestoreDatabase.Database()
+                    .Collection("public")
+                    .Document("sample1")
+            });
 
             Assert.True(true);
         }
