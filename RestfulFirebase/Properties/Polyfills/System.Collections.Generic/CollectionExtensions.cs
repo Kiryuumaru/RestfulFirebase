1729080ae7a6c21f10c47e0b7c6a459663755e0c
Kiryuumaru <kiryuumaru@outlook.com>
2022-10-24 21:35:07 +0800

    Added support to .net 7

---
+++ RestfulFirebase/Properties/Polyfills/System.Collections.Generic/CollectionExtensions.cs
@@ -0,0 +1,41 @@
+﻿#if !NET7_0_OR_GREATER
+
+using RestfulFirebase;
+using System.Diagnostics.CodeAnalysis;
+using System.Collections.ObjectModel;
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace System.Collections.Generic;
+
+internal static class CollectionExtensions
+{
+    /// <summary>
+    /// Returns a read-only <see cref="ReadOnlyCollection{T}"/> wrapper
+    /// for the specified list.
+    /// </summary>
+    /// <typeparam name="T">The type of elements in the collection.</typeparam>
+    /// <param name="list">The list to wrap.</param>
+    /// <returns>An object that acts as a read-only wrapper around the current <see cref="IList{T}"/>.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="list"/> is null.</exception>
+    public static ReadOnlyCollection<T> AsReadOnly<T>(this IList<T> list)
+    {
+        return new ReadOnlyCollection<T>(list);
+    }
+
+    /// <summary>
+    /// Returns a read-only <see cref="ReadOnlyDictionary{TKey, TValue}"/> wrapper
+    /// for the current dictionary.
+    /// </summary>
+    /// <typeparam name="TKey">The type of keys in the dictionary.</typeparam>
+    /// <typeparam name="TValue">The type of values in the dictionary.</typeparam>
+    /// <param name="dictionary">The dictionary to wrap.</param>
+    /// <returns>An object that acts as a read-only wrapper around the current <see cref="IDictionary{TKey, TValue}"/>.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="dictionary"/> is null.</exception>
+    public static ReadOnlyDictionary<TKey, TValue> AsReadOnly<TKey, TValue>(this IDictionary<TKey, TValue> dictionary) where TKey : notnull
+    {
+        return new ReadOnlyDictionary<TKey, TValue>(dictionary);
+    }
+}
+
+#endif
---
+++ RestfulFirebase/Properties/Polyfills/System.ObjectModel/ReadOnlyDictionary.cs
@@ -0,0 +1,37 @@
+﻿#if !NET7_0_OR_GREATER
+
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace System.ObjectModel;
+
+internal class ReadOnlyDictionary<TKey, TValue> : IReadOnlyDictionary<TKey, TValue>
+{
+    public TValue this[TKey key] => dictionary[key];
+
+    public IEnumerable<TKey> Keys => dictionary.Keys;
+
+    public IEnumerable<TValue> Values => dictionary.Values;
+
+    public int Count => dictionary.Count;
+
+    private readonly IDictionary<TKey, TValue> dictionary;
+
+    public ReadOnlyDictionary(IDictionary<TKey, TValue> dictionary)
+    {
+        this.dictionary = dictionary;
+    }
+
+    public bool ContainsKey(TKey key) => dictionary.ContainsKey(key);
+
+    public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator() => dictionary.GetEnumerator();
+
+#pragma warning disable CS8767 // Nullability of reference types in type of parameter doesn't match implicitly implemented member (possibly because of nullability attributes).
+    public bool TryGetValue(TKey key, [MaybeNullWhen(false)] out TValue value) => dictionary.TryGetValue(key, out value);
+#pragma warning restore CS8767 // Nullability of reference types in type of parameter doesn't match implicitly implemented member (possibly because of nullability attributes).
+
+    IEnumerator IEnumerable.GetEnumerator() => dictionary.GetEnumerator();
+}
+
+#endif
